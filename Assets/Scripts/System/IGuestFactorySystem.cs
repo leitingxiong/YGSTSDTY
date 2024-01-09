ff4257bef326b2a100db5d8cb70e855db65b280b
DESKTOP-HHT666M\leitingxiong <2419375524@qq.com>
2024-01-09 22:13:14 +0800

    <fearture> add:客人生成工厂

--- Assets/Scripts/System/IGuestFactorySystem.cs
+++ Assets/Scripts/System/IGuestFactorySystem.cs
@@ -3,7 +3,7 @@ using System;
 // 定义客人接口
 public interface IGuest
 {
-    void DisplayInfo();
+    void DisplayInfo(){}
 }
 
 public class Guest : IGuest
@@ -97,40 +97,51 @@ public class DirtyCustomer : Guest,IGuest
 // 客人工厂
 public class CustomerFactory
 {
-    Random random = new Random();
-    public IGuest CreateCustomer(string customerType)
+    public void Createrandom(int num)
+    {
+        Random random = new Random();
+        CustomerFactory customerFactory = new CustomerFactory();
+        for (int i = 0; i < num; i++)
+        {
+            IGuest customer = customerFactory.CreateCustomer(random.Next(0,100));
+            customer.DisplayInfo();
+        }
+    }
+    public IGuest CreateCustomer(int random)
     {//随机生成客人
-        switch (random.Next(0,100))
+        switch (random)
         {
             case <20:
                 return new WealthyCustomer(0,"Wealthy Customer",1000,1);
             case <40:
                 return new CustomerWithItems(0,"Customer with Items",100,0);
             default:
-                throw new ArgumentException($"Invalid customer type: {customerType}");
+                throw new ArgumentException($"Invalid customer type: {random}");
         }
     }
 }
 
-// 示例用法
 class Program
 {
-    static void Main()
+    void Main()
     {
+        Random random = new Random();
         CustomerFactory customerFactory = new CustomerFactory();
-
+        
+        
         // 创建富有的客人
-        IGuest wealthyCustomer = customerFactory.CreateCustomer("wealthy");
+        IGuest wealthyCustomer = customerFactory.CreateCustomer(random.Next(0,100));
         wealthyCustomer.DisplayInfo();
 
         // 创建怪物客人
-        IGuest monsterCustomer = customerFactory.CreateCustomer("monster");
+        IGuest monsterCustomer = customerFactory.CreateCustomer(random.Next(0,100));
         monsterCustomer.DisplayInfo();
 
         // 创建不干净的客人
-        IGuest dirtyCustomer = customerFactory.CreateCustomer("dirty");
+        IGuest dirtyCustomer = customerFactory.CreateCustomer(random.Next(0,100));
         dirtyCustomer.DisplayInfo();
 
-        // ... 可以继续创建其他类型的客人
     }
+    //根据需要的数量生成随机客人
+   
 }
